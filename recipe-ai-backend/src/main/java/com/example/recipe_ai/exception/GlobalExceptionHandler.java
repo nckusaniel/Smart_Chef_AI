d999c231445edf6646223dd749af66a7fec41c20
daniel <a3061515@gmail.com>
2025-11-08 10:58:10 +0800

    feat:整理code

--- recipe-ai-backend/src/main/java/com/example/recipe_ai/exception/GlobalExceptionHandler.java
+++ recipe-ai-backend/src/main/java/com/example/recipe_ai/exception/GlobalExceptionHandler.java
@@ -1,15 +1,12 @@
 package com.example.recipe_ai.exception;
-// ResponseEntity 代表一個完整的 HTTP 回應，它包含了 HTTP 狀態碼、標頭（Headers）以及回應主體（Body）。
-// 我們用它來精確控制要回傳給前端的內容和狀態碼。
+// ResponseEntity 代表一個完整的 HTTP 回應，包含HTTP狀態、標頭、Body。
 import org.springframework.http.ResponseEntity;
-// import @ExceptionHandler 註解，告訴 Spring，下面的這個方法用來處理特定種類的異常（Exception）
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.Map;
 
-// @RestControllerAdvice 告訴 Spring Boot：「請掃描這個類別，把它當作一個全域的異常處理器」。
-// 任何Controller拋出異常時，Spring 都會來這個類別裡尋找對應方法
 @RestControllerAdvice
+// @RestControllerAdvice是全域的異常處理器。有異常時，Spring來這個類別找方法
 public class GlobalExceptionHandler {
     // 如果專案中出現ApiException，執行下面方法，e是 ApiException，e可以 .getMessage()、 getStatus()。
     @ExceptionHandler(ApiException.class)
